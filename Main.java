1a6d16b0b359144cc259d9702eb5945678d1033e
downstize <swankydid@gmail.com>
2023-10-24 19:01:19 +0300

    some changes (3rd commit from dev branch)

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Первый коммит с ветки dev");
         System.out.print("Второй коммит с ветки dev");
+        System.out.print("Третий коммит с ветки dev");
     }
 }
