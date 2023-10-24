c2e9a279e2546b0957a2d19b3e46cc73b3fbbebe
downstize <swankydid@gmail.com>
2023-10-24 19:01:39 +0300

    some changes (4th commit from dev branch)

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main{
         System.out.print("Первый коммит с ветки dev");
         System.out.print("Второй коммит с ветки dev");
         System.out.print("Третий коммит с ветки dev");
+        System.out.print("Четвёртый коммит с ветки dev");
     }
 }
